57d810c7decf61eb6bb828721d377c1f6a1c6f41
SamuraiZach <81524537+SamuraiZach@users.noreply.github.com>
2022-11-27 12:52:13 -0500

    rip

--- src/index.tsx
+++ src/index.tsx
@@ -24,7 +24,7 @@
 						<span>Zachariah Barraza, Tavon Gage, Michael King</span>
 					</div>
 					<div style={{ position: "absolute", left: "23%", top: "50px" }}>
-						<img src={require("./worldmap2.png")}  height={750} width={1200} />
+						<img src={require("./worldmap2.png")} alt="oops mad no load teehee" height={750} width={1200} />
 					</div >
 					<div style={{ position: "absolute", left: "23%", top: "50px", height: "750px", width:"1200px" }}><Example /></div>
 				</DndProvider>
